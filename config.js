9043a485a2b6d37683e8692a652b80fbfa4495c5
Ngminhuw <86766680+Ngminhuw@users.noreply.github.com>
2021-07-01 14:04:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Babi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ iêu đây là thương`,
+    btnIntro: 'hé hé',
+    title: 'Bắt đền đó nha. Người ta bảo đâu ai bình thường khi yêu. Em giờ chả khác gì người điên cả ',
+    desc: 'Phải chăng em điên đảo và quá thương anh ',
+    btnYes: 'Anh cũng thương em nhiều nhiều nhiều ',
+    btnNo: 'Em là người điên á! Thôi anh không iêu đâu',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chọn em :> ',
+    btnReply: 'Gửi cho em ^^',
+    reply: 'Bởi vì em là chính em ^^ ',
+    mess: 'Em biết mà 🥰. Iêu anh nhiều nhiều 😘😘',
+    messDesc: 'Tối nào cũng phải trả bài "iêu em, thương em" đó nha ',
+    btnAccept: 'Oker nuôn',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
